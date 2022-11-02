4cf9779eed0cfe0585084d87dc6a6ff8848eaa5f
Przemek Miros <kontakt@futurewebstudio.pl>
2022-11-02 10:44:17 +0100

    Add contact  form

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/assets/css");
     eleventyConfig.addPassthroughCopy("./src/assets/js");
     eleventyConfig.addPassthroughCopy("./src/assets/img");
     eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+    eleventyConfig.addWatchTarget("./src/assets/css/");
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
