ed8afe88593f7bc79d9710d6e0d72558af36ea65
Przemek Miros <kontakt@futurewebstudio.pl>
2022-11-02 09:38:32 +0100

    Add fonts

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/css");
     eleventyConfig.addPassthroughCopy("./src/assets/js");
     eleventyConfig.addPassthroughCopy("./src/assets/img");
+    eleventyConfig.addPassthroughCopy("./src/assets/fonts");
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
