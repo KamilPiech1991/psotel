65108e5516448074148891582001925c7b141830
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-02-20 11:52:12 +0100

    Lazy load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -97,3 +97,16 @@ window.addEventListener('scroll', function() {
 	   contentNo++;
 	});
 
+// Accordion
+if (document.querySelector(".accordion")) {
+	let t = document.getElementsByClassName("accordion");
+	  for (let e = 0; e < t.length; e++) t[e].addEventListener("click", function () {
+		let e = this.nextElementSibling;
+		if (e.style.maxHeight) e.style.maxHeight = null, this.classList.remove("open");
+		else {
+		  for (let a = 0; a < t.length; a++) t[a].classList.remove("open"), t[a].nextElementSibling.style.maxHeight = null;
+		  e.style.maxHeight = e.scrollHeight + "px", this.classList.toggle("open");
+		}
+	  });
+	};
+  
